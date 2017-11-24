43ed33ab31ebc2c6fd5a3f0bccfc13c75903e110
Andrew Wreggelsworth <andwregg@umich.edu>
2017-11-23 20:45:45 -0500

    Changes based on comments

--- user_profile_jscript.js
+++ user_profile_jscript.js
@@ -1,28 +1,15 @@
-//var $ = require('jquery');
-$(document).ready(function(){
-  var portfolioText;
-  var emailText;
-  $(".editInfo").click(function(){
-    $(".portfolioInput").prop("hidden", false);
-    $(".emailInput").prop("hidden", false);
-    $(".socialMedia").prop("hidden", false);
-    $(".saveUserInfo").prop("hidden", false);
-    $(".editInfo").prop("hidden", true);
-    $(".portfolioLink").prop("hidden", true);
-    $(".emailOutput").prop("hidden", true);
-  });
-  $(".saveUserInfo").click(function(){
-    portfolioText = $(".portfolioInput").val();
-    $(".portfolioLink").text(portfolioText);
-    $(".portfolioLink").attr("href", portfolioText);
-    emailText = $(".emailInput").val();
-    $(".emailOutput").val(emailText);
-    $(".portfolioInput").prop("hidden", true);
-    $(".emailInput").prop("hidden", true);
-    $(".socialMedia").prop("hidden", true);
-    $(".saveUserInfo").prop("hidden", true);
-    $(".editInfo").prop("hidden", false);
-    $(".portfolioLink").prop("hidden", false);
-    $(".emailOutput").prop("hidden", false);
-  });
+var portfolioText;
+var emailText;
+$("#editInfo").click(function(){
+  $(".editProfile").prop("hidden", false);
+  $(".normalProfile").prop("hidden", true);
+});
+$("#saveUserInfo").click(function(){
+  portfolioText = $("#portfolioInput").val();
+  $("#portfolioLink").text(portfolioText);
+  $("#portfolioLink").attr("href", portfolioText);
+  emailText = $("#emailInput").val();
+  $("#emailOutput").val(emailText);
+  $(".editProfile").prop("hidden", true);
+  $(".normalProfile").prop("hidden", false);
 });
