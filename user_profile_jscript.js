9d9498f8d15a8d35636168ce1076e4ed3f49017e
Andrew Wreggelsworth <andwregg@umich.edu>
2017-11-27 21:27:07 -0500

    Changed user_profile js file

--- user_profile_jscript.js
+++ user_profile_jscript.js
@@ -1,14 +1,12 @@
-var portfolioText;
-var emailText;
 $("#editInfo").click(function(){
   $(".editProfile").prop("hidden", false);
   $(".normalProfile").prop("hidden", true);
 });
 $("#saveUserInfo").click(function(){
-  portfolioText = $("#portfolioInput").val();
+  var portfolioText = $("#portfolioInput").val();
   $("#portfolioLink").text(portfolioText);
   $("#portfolioLink").attr("href", portfolioText);
-  emailText = $("#emailInput").val();
+  var emailText = $("#emailInput").val();
   $("#emailOutput").val(emailText);
   $(".editProfile").prop("hidden", true);
   $(".normalProfile").prop("hidden", false);
